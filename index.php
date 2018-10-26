ff6bf3fafae76252573d84c297b0b9b35ac1d62c
Samuel Woniowei <xcellentsam@gmail.com>
2018-10-26 14:43:04 +0100

    Added project folders

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Fuocribs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+
+
+</head>
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">FUOCRIBS</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#">LOGIN</a></li>
+        <li><a href="#">SIGNUP</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<script src="js/jquery/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
