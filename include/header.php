74b8bd8d84a702e67fb885a8d13e515938e495e7
Samuel Woniowei <xcellentsam@gmail.com>
2018-10-28 04:10:20 +0100

    header component

---
+++ include/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Fuocribs</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/custom.css">
+
+</head>
+<body>
