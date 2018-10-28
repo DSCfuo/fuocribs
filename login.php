8e6ad797501b1231b2442785fa02f164a2743f97
Samuel Woniowei <xcellentsam@gmail.com>
2018-10-28 02:18:43 +0100

    Added correct connect php link

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php
-//require_once 'connect.php';
+//require_once 'db/connect.php';
 session_start();
 
 if (isset($_POST['submit'])) {
@@ -7,16 +7,16 @@ $user =  mysqli_real_escape_string($conn, $_POST['username']);
 $pwd =  mysqli_real_escape_string($conn, $_POST['password']);
 
 
-                                      $query = "SELECT username, password FROM login WHERE password='".md5($pwd)."' AND username='$user'";
-                                      $sql = mysqli_query($conn,$query);
-                                               if (mysqli_num_rows($sql) == 1)
-                                            {
-																							echo '<script>window.location.href = "../home"</script>';
-                                              $_SESSION['email'] = $email;
-                                           }
-                                          else
-                                          {
-                                          echo $error;
-                                          }
-																				}
+$query = "SELECT username, password FROM login WHERE password='".md5($pwd)."' AND username='$user'";
+$sql = mysqli_query($conn,$query);
+ if (mysqli_num_rows($sql) == 1)
+  {
+    echo '<script>window.location.href = "../home"</script>';
+    $_SESSION['email'] = $email;
+  }
+  else
+    {
+      echo $error;
+    }
+  }
  ?>
